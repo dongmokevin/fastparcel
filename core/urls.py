e9fbbe327fb61de65ff6cd8a9047533a3c1458be
Kevin Dongmo <shaminseta@gmail.com>
2022-02-22 22:44:11 -0500

    commit Authentication

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home),
     path('customer/', views.customer_page),
     path('courier/', views.courier_page),
+    path('sign-up/', views.sign_up),
 ]
--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -17,10 +17,13 @@ from multiprocessing.dummy import Namespace
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
     path('', include('core.urls')),
 ]
 
