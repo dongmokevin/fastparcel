bd91c55e57f2223dac47acc0197d14d1c8e3353e
Kevin Dongmo <shaminseta@gmail.com>
2022-02-22 20:19:29 -0500

    Initialize project

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.home),
+    path('customer/', views.customer_page),
+    path('courier/', views.courier_page),
+]
